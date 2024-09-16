f9ccd7cf1de7028c19a2fc0071e757c1dd96e584
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplsencia.esm>
2024-09-16 20:23:47 +0200

    Ejercicio 1 Terminado

--- Tema 3/cadenas1.php
+++ Tema 3/cadenas1.php
@@ -1,40 +1,68 @@
-<!doctype html>
-<html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <title>Cadenas 1</title>
-    </head>
-    <body>
-        <?php
-            //1
-            $nombre = 'anaMArial';
+<?php
+    //1
+    $nombre = 'anaMArial';
 
-            //2
-            echo $nombre, "<br>";
+    //2
+    echo $nombre, "<br>";
 
-            //3
-            echo strlen($nombre), "<br>";
+    //3
+    echo strlen($nombre), "<br>";
 
-            //4
-            echo strtoupper($nombre) , "<br>";
+    //4
+    echo strtoupper($nombre), "<br>";
+
+    //5
+    echo strtolower($nombre), "<br>";
+
+    //6
+    echo substr_count($nombre, "A"), "<br>";
+
+    //7
+    echo ord($nombre[0]), "<br>";
+    
+    //8
+    // BUSCO LA PRIMERA POSCIÓN QUE CONTENGA UNA (A - a)
+    $posicionPrimeraA = stripos($nombre, 'a');
+
+    //SI (posicionPrimeraA) = FALSE RETORNO -1, SI NO MUESTRO LA POSICIÓN
+    if ($posicionPrimeraA === false) {
+        echo -1;
+    } else {
+        echo $posicionPrimeraA;
+    }
+
+    echo "<br>";
+    
+    //9
+    $posicionUltimaA = strripos($nombre, 'a');
+
+    //SI (posicionUltimaA) = FALSE RETORNO -1, SI NO MUESTRO LA POSICIÓN
+    if ($posicionUltimaA === false) {
+        echo -1;
+    } else {
+        echo $posicionUltimaA;
+    }
+
+    echo "<br>";
         
-            //5
-            echo strtolower($nombre) , "<br>";
+    //10
+    $nombreSustituido = str_ireplace('a', '0', $nombre);
+    echo $nombreSustituido, "<br>";
+    
+    //11
+    if (stripos($nombre, 'al') === 0) {
+        echo "El nombre comienza por 'al'";
+    } else {
+        echo "El nombre no comienza por 'al'";
+    }
 
-            //6
-            echo substr_count($nombre, "A") , "<br>";
+    echo "<br>";
 
-            //7 muestra el codgio ascci de la primera letra del nombre
-            echo ord(), "<br>";
-            
-            
-
-            //8 muestra la posicion de la primera a existante en el nombre sea mayuscula o minuscula , si no hay nunguna mostrará -1
-            //9 lo mismo pero con a última a.
-            //10
-            //11
-            //12
-            //13
-        ?>
-    </body>
-</html>
+    //12
+    $nombreSinEspacios = trim($nombre);
+    echo $nombreSinEspacios, "<br>";
+        
+    //13
+    $nombrePrimeraMayuscula = ucfirst(strtolower($nombre));
+    echo $nombrePrimeraMayuscula, "<br>";
+?>
