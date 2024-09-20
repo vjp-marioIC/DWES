b9f98b4fdf891e3445dd9d09d37439816f036b46
Mario Iglesias Capa <marioib1@informatica.iesvalledeljerteplsencia.esm>
2024-09-20 14:01:06 +0200

    Ejercicio1 de array Termiado

--- Tema 3/Arrays/arrays1.php
+++ Tema 3/Arrays/arrays1.php
@@ -22,9 +22,9 @@
             echo $cadenaNombres . "<br>";
 
             //4
-            $arr = $nombres;    
-            sort($arr);
-            print_r($arr);
+            $arryOrdenado = $nombres;  
+            sort($arryOrdenado);
+            print_r($arryOrdenado);
 
             echo "<br>";
             
@@ -34,18 +34,14 @@
 
             echo "<br>";
             
-            /*
             //6
-            $posicionNombre = array_search('Mario', $nombre);
+            $posicionNombre = array_search('Hugo', $nombres);
             
-            if ($posicionNombre !== null) {
+            if ($posicionNombre !== false) {
                 echo "Valor encontrado con la clave: " . $posicionNombre . "<br>";
             } else {
                 echo "Valor no encontrado" . "<br>";
             }
-            
-            print_r($posicionNombre);
-            */
             
             //7
             $arrayAleatorio = $nombres;
@@ -53,7 +49,53 @@
             print_r($arrayAleatorio);
             
             //8
-            
+            foreach ($nombres as $nombre) {
+                if (strpos($nombre, 'a') !== false) {
+                    echo $nombre . " ";
+                }
+            }
+
+            echo "<br>";
+
+            //9
+            $alumnos = [
+                ['id' => 1, 'nombre' => 'Mario', 'edad' => 22],
+                ['id' => 2, 'nombre' => 'Dani', 'edad' => 22],
+                ['id' => 3, 'nombre' => 'Hugo', 'edad' => 21],
+                ['id' => 4, 'nombre' => 'Jose', 'edad' => 21],
+                ['id' => 5, 'nombre' => 'Nacho', 'edad' => 33]
+            ];
+
+            //10
+            echo "<table border='1'>";
+            echo "<tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Edad</th> 
+                 </tr>";
+
+            foreach ($alumnos as $alumno) {
+                echo "<tr>
+                        <td>{$alumno['id']}</td>
+                        <td>{$alumno['nombre']}</td>
+                        <td>{$alumno['edad']}</td>
+                      </tr>";
+            }
+
+            echo "</table>";
+
+            //11
+            $arrySoloNombres = array_column($alumnos, 'nombre');
+            print_r($arrySoloNombres);
+
+            echo "<br>";
+
+            //12
+            $arryNumeros = [1, 2, 3 , 4, 5 , 6 , 7, 8, 9, 10];
+            $arraySumaNumeros = array_sum($arryNumeros);
+
+            echo "La suma de todos los números del array es: ";
+            print_r($arraySumaNumeros);
         ?>
     </body>
 </html>
